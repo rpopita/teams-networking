6a9a337b13f2d5b0c30923863341c638dd550016
rpopita <robert.popita@linnify.com>
2023-07-05 18:16:28 +0300

    render teams

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./style.css";
 
 console.warn("start app");
+
+function getTeamAsHTML(team) {
+  return ` <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${teams.name}</td>
+    <td>${team.url}</td>
+    <td>x</td>
+   </tr>`;
+}
+
+function renderTeams(teams) {
+  const htmlTeams = teams.map(getTeamsAsHTML);
+  console.warn(htmlTeams);
+  document.querySelectorAll("#teamsTable tbody").innerHTML = htmlTeams.join("");
+}
+
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
